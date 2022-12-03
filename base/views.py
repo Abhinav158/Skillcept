9c6db3327fee0321c979e66bd2e96d76bcaf163d
Abhinav <raghunandan.abhinav@gmail.com>
2022-12-03 16:36:14 +0530

    changed url paths and added app template structure

--- base/views.py
+++ base/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 
-# This request tells us what kind of data is going to be passed in to the backend by the user 
+# The request parameter tells us what kind of data is going to be passed in to the backend by the user 
+
+rooms = [
+    {'id': 1, 'name': 'Learn ReactJS'},
+    {'id': 2, 'name': 'NodeJS Crash Course'},
+    {'id': 3, 'name': 'Django Developers'},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
