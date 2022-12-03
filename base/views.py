6f251e1bcc5d5637204c162bbe22326304f9a175
Abhinav <raghunandan.abhinav@gmail.com>
2022-12-04 00:29:57 +0530

    Implemented comment section - few bugs present

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
 
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 # The request parameter tells us what kind of data is going to be passed in to the backend by the user 
 
@@ -82,8 +82,22 @@ def home(request):
 
 
 def room(request, pk):
-    room = Room.objects.get(id=pk)    
-    context = {'room': room}
+    room = Room.objects.get(id=pk) 
+    # Get the set of messages related to one particular room 
+    # Display the comment list with the most recent message on top
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    if request.method == 'POST':
+        # Go ahead and create a message  
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        # Add the current user to the many to many field 
+        room.participants.add(request.user)
+        return redirect('room',pk=room.id)  
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
 # We want to remove the Create Room option if the user has not logged in
